f8950c9089c75dc3d3f7265cc9daabe5eed6667c
j4a1cksn <jacksonrojas222@gmail.com>
2023-06-04 10:54:20 -0500

    funcion de setCanvasSize
    lo que hace es que el canvas sea responsiv y se dapte a las pantallas y que su contenido tambien

--- game.js
+++ game.js
@@ -1,9 +1,36 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d')
 
-window.addEventListener('load',startGame);
+let canvasSize;
+let elemnetSize;
+
+window.addEventListener('load',setCanvasSize);
+window.addEventListener('resize',setCanvasSize);
+
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    }else{
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+    
+    elemnetSize = (canvasSize / 10);
+
+
+    startGame();
+}
 
 function startGame() {
-    //game.fillRect(0,50,100,100);
-    game.fillText('goku', 10,10)
+
+    game.font = elemnetSize + 'px Verdana';
+    game.textAlign = "end";
+
+    for (let i = 1; i <= 10 ; i++) {
+        game.fillText(emojis['X'], elemnetSize * i + 15, elemnetSize);
+    }
+
 }
